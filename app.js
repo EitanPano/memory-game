79f6b11c2095903108956633f565dc9d0f4bea6e
Eitan Pano <eitanpano95@gmail.com>
2021-08-27 10:33:17 +0300

    add html data attribute to each card, added js logic

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const cards = document.querySelectorAll('.card');
 
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+let isFlippedCard = false;
+let secondCard, firstCard;
+
 function flipCard() {
     this.classList.toggle('flipped');
     this.classList.toggle('face-down');
+    
+    if(!isFlippedCard) {
+        // first click
+        isFlippedCard = true;
+        firstCard = this;
+    }
+    else {
+        // second click
+        isFlippedCard = false;
+        secondCard = this;
+
+        // check if cards match.
+        console.log({firstCard, secondCard});
+    }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
