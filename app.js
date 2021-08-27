554fafbc18a0aaeceb9434b253ecab4e61c37458
Eitan Pano <eitanpano95@gmail.com>
2021-08-27 13:03:28 +0300

    Completed js logic, added sounds.

--- app.js
+++ app.js
@@ -1,45 +1,64 @@
 const cards = document.querySelectorAll('.card');
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+const shuffle = (() => {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() *12);
+        card.style.order = randomPos;
+        card.addEventListener('click', flipCard);
+    })
+})()
 
+// cards.forEach(card => card.addEventListener('click', flipCard));
 
-let isFlippedCard = false;
+let isFlippedCard = true;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.toggle('flipped');
     this.classList.toggle('face-down');
-    
-    if(!isFlippedCard) {
-        // first click
-        isFlippedCard = true;
-        firstCard = this;
-    }
-    else {
-        // second click
+
+    if (isFlippedCard) {
+        // first card click
         isFlippedCard = false;
-        secondCard = this;
+        firstCard = this;
 
-        checkForMatch();
+        return;
     }
-}
+    // second card click
+    isFlippedCard = true;
+    secondCard = this;
 
+    checkForMatch();
+}
 
 // check if cards match.
 const checkForMatch = () => {
-    if(firstCard.dataset.mark === secondCard.dataset.mark) {
-        console.log('Right Couple');
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-    }
-    else {
-        console.log('Wrong Couple');
-        setTimeout(() => {
-            firstCard.classList.toggle('flipped');
-            firstCard.classList.toggle('face-down');
-            secondCard.classList.toggle('flipped');
-            secondCard.classList.toggle('face-down');
-        }, 1200)
-    }
+    const isMatch = firstCard.dataset.mark === secondCard.dataset.mark;
+
+    isMatch ? disableCards() : unflipCards();
 }
+
+// Cards match, disable them
+const disableCards = () => {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+// Wrong couple flipped, unflip.
+const unflipCards = () => {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.toggle('flipped');
+        firstCard.classList.toggle('face-down');
+        secondCard.classList.toggle('flipped');
+        secondCard.classList.toggle('face-down');
+
+        lockBoard = false;
+    }, 1200)
+}
+
 
